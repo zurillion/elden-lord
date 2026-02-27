b65a6b4f549d148cd0bd22df534f2d351f943a97
giocaristi <zurillion@mac.com>
2026-02-27 02:40:45 +0100

    fix(ui): remove FOUC injected body CSS after window load so custom themes can apply fully

--- docs/js/common.js
+++ docs/js/common.js
@@ -14,43 +14,43 @@ window.LANGUAGE_META = {
 window.currentLanguage = localStorage.getItem('selectedLanguage') || 'en';
 
 var themes = {
-    "Standard" : "/css/bootstrap.min.css",
-    "LightMode" : "/css/themes/lightmode/bootstrap.min.css",
-    "Ceruleon" : "/css/themes/cerulean/bootstrap.min.css",
-    "Cosmo" : "/css/themes/cosmo/bootstrap.min.css",
-    "Cyborg" : "/css/themes/cyborg/bootstrap.min.css",
-    "Darkly" : "/css/themes/darkly/bootstrap.min.css",
-    "Flatly" : "/css/themes/flatly/bootstrap.min.css",
-    "Journal" : "/css/themes/journal/bootstrap.min.css",
-    "Litera" : "/css/themes/litera/bootstrap.min.css",
-    "Lumen" : "/css/themes/lumen/bootstrap.min.css",
-    "Lux" : "/css/themes/lux/bootstrap.min.css",
-    "Materia" : "/css/themes/materia/bootstrap.min.css",
-    "Minty" : "/css/themes/minty/bootstrap.min.css",
-    "Morph" : "/css/themes/Morph/bootstrap.min.css",
-    "Pulse" : "/css/themes/pulse/bootstrap.min.css",
-    "Quartz" : "/css/themes/quartz/bootstrap.min.css",
-    "Regent" : "/css/themes/regent/bootstrap.min.css",
-    "Sandstone" : "/css/themes/sandstone/bootstrap.min.css",
-    "Simplex" : "/css/themes/simplex/bootstrap.min.css",
-    "Sketchy" : "/css/themes/sketchy/bootstrap.min.css",
-    "Slate" : "/css/themes/slate/bootstrap.min.css",
-    "Solar" : "/css/themes/solar/bootstrap.min.css",
-    "Spacelab" : "/css/themes/spacelab/bootstrap.min.css",
-    "Superhero" : "/css/themes/superhero/bootstrap.min.css",
-    "United" : "/css/themes/united/bootstrap.min.css",
-    "Vapor" : "/css/themes/vapor/bootstrap.min.css",
-    "Yeti" : "/css/themes/yeti/bootstrap.min.css",
-    "Zephyr" : "/css/themes/zephyr/bootstrap.min.css",
+    "Standard": "/css/bootstrap.min.css",
+    "LightMode": "/css/themes/lightmode/bootstrap.min.css",
+    "Ceruleon": "/css/themes/cerulean/bootstrap.min.css",
+    "Cosmo": "/css/themes/cosmo/bootstrap.min.css",
+    "Cyborg": "/css/themes/cyborg/bootstrap.min.css",
+    "Darkly": "/css/themes/darkly/bootstrap.min.css",
+    "Flatly": "/css/themes/flatly/bootstrap.min.css",
+    "Journal": "/css/themes/journal/bootstrap.min.css",
+    "Litera": "/css/themes/litera/bootstrap.min.css",
+    "Lumen": "/css/themes/lumen/bootstrap.min.css",
+    "Lux": "/css/themes/lux/bootstrap.min.css",
+    "Materia": "/css/themes/materia/bootstrap.min.css",
+    "Minty": "/css/themes/minty/bootstrap.min.css",
+    "Morph": "/css/themes/Morph/bootstrap.min.css",
+    "Pulse": "/css/themes/pulse/bootstrap.min.css",
+    "Quartz": "/css/themes/quartz/bootstrap.min.css",
+    "Regent": "/css/themes/regent/bootstrap.min.css",
+    "Sandstone": "/css/themes/sandstone/bootstrap.min.css",
+    "Simplex": "/css/themes/simplex/bootstrap.min.css",
+    "Sketchy": "/css/themes/sketchy/bootstrap.min.css",
+    "Slate": "/css/themes/slate/bootstrap.min.css",
+    "Solar": "/css/themes/solar/bootstrap.min.css",
+    "Spacelab": "/css/themes/spacelab/bootstrap.min.css",
+    "Superhero": "/css/themes/superhero/bootstrap.min.css",
+    "United": "/css/themes/united/bootstrap.min.css",
+    "Vapor": "/css/themes/vapor/bootstrap.min.css",
+    "Yeti": "/css/themes/yeti/bootstrap.min.css",
+    "Zephyr": "/css/themes/zephyr/bootstrap.min.css",
 };
 
 
-(function($) {
+(function ($) {
     'use strict';
 
     window.profiles = $.jStorage.get(profilesKey, {});
-    
-    window.initializeProfile = function(profile_name) {
+
+    window.initializeProfile = function (profile_name) {
         if (!(profile_name in profiles[profilesKey])) profiles[profilesKey][profile_name] = {};
         if (!('checklistData' in profiles[profilesKey][profile_name]))
             profiles[profilesKey][profile_name].checklistData = {};
@@ -70,14 +70,14 @@ var themes = {
             profiles[profilesKey][profile_name].dlc_filter = 'both';
         $.jStorage.set(profilesKey, profiles);
     }
-    
+
     window.themeSetup = function (stylesheet) {
-        if(stylesheet === null || stylesheet === undefined) { // if we didn't get a param, then
+        if (stylesheet === null || stylesheet === undefined) { // if we didn't get a param, then
             stylesheet = profiles[profilesKey][profiles.current].style; // fall back on "light" if cookie not set
         }
         $("#bootstrap").attr("href", themes[stylesheet]);
     }
-    
+
     /// assure default values are set
     /// necessary 'cause we're abusing local storage to store JSON data
     /// done in a more verbose way to be easier to understand
@@ -85,10 +85,10 @@ var themes = {
     if (!(profilesKey in profiles)) profiles[profilesKey] = {};
     initializeProfile(profiles.current);
     window.themeSetup(profiles[profilesKey][profiles.current].style);
-    
+
 
     if ('loading' in HTMLImageElement.prototype) {
-        $('img[loading="lazy"]').each(function() {
+        $('img[loading="lazy"]').each(function () {
             $(this).attr('src', this.dataset.src);
         });
     } else {
@@ -103,7 +103,7 @@ var themes = {
         console.log(t)
         t.addClass('border border-primary border-3');
     }
-        
+
     function setCheckbox(id, checked) {
         if ($('#' + id).length === 1) {
             var el = $('#' + id).get(0);
@@ -180,7 +180,7 @@ var themes = {
             $('.lang-text.lang-' + lang).removeClass('d-none');
             // Fallback: for lang-pair elements that have no target-language child,
             // fall back to showing the English span.
-            $('.lang-pair').each(function() {
+            $('.lang-pair').each(function () {
                 if (!$(this).children('.lang-text.lang-' + lang).length) {
                     $(this).children('.lang-text.lang-en').removeClass('d-none');
                 }
@@ -197,7 +197,7 @@ var themes = {
         $('.lang-option[data-lang="' + lang + '"]').addClass('active');
     }
 
-    window.setLanguage = function(lang) {
+    window.setLanguage = function (lang) {
         window.currentLanguage = lang;
         localStorage.setItem('selectedLanguage', lang);
         applyLanguageCss(lang);
@@ -209,10 +209,14 @@ var themes = {
     applyLanguageCss(window.currentLanguage);
 
     // Language selector click handler
-    $(document).on('click', '.lang-option', function(e) {
+    $(document).on('click', '.lang-option', function (e) {
         e.preventDefault();
         window.setLanguage($(this).data('lang'));
     });
 
-})( jQuery );
-    
+    $(window).on('load', function () {
+        var themeFix = document.getElementById('theme-fouc-fix');
+        if (themeFix) themeFix.remove();
+    });
+
+})(jQuery);
